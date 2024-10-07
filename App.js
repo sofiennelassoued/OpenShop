906c291666d71160a7bf543609af5446835f53cc
Sofienne Lassoued <sofienne@hexis.live>
2024-10-07 19:21:34 +0100

    Formatted code

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import AppNavigator from './src/navigation/AppNavigator';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-
-      <AppNavigator/>
+      <AppNavigator />
 
       <StatusBar style="auto" />
-      
     </SafeAreaView>
   );
 }
@@ -18,6 +16,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
